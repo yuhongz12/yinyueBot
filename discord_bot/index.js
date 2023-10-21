11ddaa263d329d901aee5a3f7c4ad636d24aab1f
Aaron Lin <aaron.lin.2@stonybrook.edu>
2023-10-21 16:50:50 -0400

    Bot Online

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -1,7 +1,40 @@
-const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client{(
-    Intents:[
-        
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
-)};
+});
+
+client.on("ready", () => { 
+    console.log("Bot Online!");
+})
+
+client.on("messageCreate", async (message) => {
+    if (message.content === "ping") {
+        message.reply({
+            content: "pong"
+        })
+    }
+})
+
+client.login(process.env.DISCORD_TOKEN)
+
+client.on("messageCreate", async (message, ...nums) => {
+    if (message.content.startsWith("add")) {
+        const args = message.content.slice("add".length).trim().split(' ');
+        console.log(args)
+        total = 0
+        for (var numStr of args) {
+            total += parseInt(numStr, 10)
+        }
+        console.log(total)
+        message.reply({
+            content: `${total}`
+        })
+    }
+})
